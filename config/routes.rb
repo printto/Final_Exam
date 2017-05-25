7bfbc0655bf93df190e185ed9ed88180899757e9
Pappim Pipatkasrira <pappim.p@ku.th>
2017-05-25 07:45:29 +0000

    edit footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
     root 'static_pages#home'
     get '/help', to: 'static_pages#help'
     get '/tuition', to: 'static_pages#tuition'
-    get '/contactus', to: 'static_pages#contact'
+    get '/contactus', to: 'static_pages#contactus'
     get '/academic', to: 'static_pages#academic'
 end
