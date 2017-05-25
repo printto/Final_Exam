8fa9c94710cc82080d829d868b62fb83cc3d3906
Pappim Pipatkasrira <pappim.p@ku.th>
2017-05-25 07:40:48 +0000

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
-get 'static_pages/home'
-
-get 'static_pages/help'
-
-root 'application#hello'
-
+    root 'static_pages#home'
+    get '/help', to: 'static_pages#help'
+    get '/tuition', to: 'static_pages#tuition'
+    get '/contactus', to: 'static_pages#contact'
+    get '/academic', to: 'static_pages#academic'
 end
